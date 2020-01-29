95f42b27f4abd328bb89537f43a4cd699f6ad9e8
petar <petar@aktek.io>
2020-01-29 17:04:11 +0100

    redo play again menu

--- components/Loaded/PlayAgainMenu/PlayAgainMenu.js
+++ components/Loaded/PlayAgainMenu/PlayAgainMenu.js
@@ -1,30 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Image } from "react-native";
+import { Animated, Easing } from "react-native";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const PlayAgainMenu = ({ handlePlayAgain }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.overlay}>
-        <View style={styles.messageContainer}>
-          <Text style={styles.message}>you died, loser</Text>
-        </View>
-        <View style={styles.lotusContainer}>
-          <Image
-            source={require("../../../assets/images/lotus.png")}
-            style={styles.player}
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={handlePlayAgain}>
-            <Text style={styles.button}>reincarnate</Text>
-          </TouchableOpacity>
+class PlayAgainMenu extends Component {
+  animatedValue = new Animated.Value(0);
+
+  componentDidMount() {
+    this.scaleView();
+  }
+
+  scaleView = () => {
+    Animated.timing(this.animatedValue, {
+      toValue: 1,
+      duration: 4000,
+      easing: Easing.bounce,
+      useNativeDriver: true
+    }).start();
+  };
+
+  render() {
+    const { handlePlayAgain } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.overlay}>
+          <View style={styles.messageContainer}>
+            <Text style={styles.message}>you died, loser</Text>
+          </View>
+          <View style={styles.lotusContainer}>
+            <Animated.Image
+              source={require("../../../assets/images/lotus.png")}
+              style={{
+                transform: [
+                  {
+                    translateX: this.animatedValue.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, 1]
+                    })
+                  },
+                  {
+                    translateY: this.animatedValue.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, 1]
+                    })
+                  },
+                  {
+                    scaleX: this.animatedValue.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0.2, 0.8]
+                    })
+                  },
+                  {
+                    scaleY: this.animatedValue.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0.2, 0.8]
+                    })
+                  }
+                ]
+              }}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={handlePlayAgain}>
+              <Text style={styles.button}>reincarnate</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default PlayAgainMenu;
 
@@ -36,12 +81,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
     zIndex: 1
   },
   overlay: {
     width: "75%",
     height: "50%",
-    backgroundColor: "black",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
